0ec8624dfadbd6bd7d721792a770d8443509f06d
Dorian Kita <dorian.kita90@gmail.com>
2024-04-09 21:47:25 +0200

    added custom validation for password matches

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -6,6 +10,11 @@ export default function Signup() {
     const data = Object.fromEntries(fd.entries());
     const acquisitionChannel = fd.getAll("acquisition");
     data.acquisition = acquisitionChannel;
+
+    if (data.password !== data["confirm-password"]) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
     console.log(data);
   }
 
@@ -39,6 +48,9 @@ export default function Signup() {
             name="confirm-password"
             required
           />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords are not equal</p>}
+          </div>
         </div>
       </div>
 
